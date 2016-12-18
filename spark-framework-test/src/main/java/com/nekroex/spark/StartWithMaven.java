39a6bffbef044ab53d0f211533e3abc26d5729f5
竹箐 <jiyong.jy@alibaba-inc.com>
2016-12-18 15:42:00 +0800

    easy start

---
+++ spark-framework-test/src/main/java/com/nekroex/spark/StartWithMaven.java
@@ -0,0 +1,17 @@
+package com.nekroex.spark;
+
+import static spark.Spark.*;
+/**
+ * <p>
+ *    start with maven
+ * </p>
+ * <p> Date: 2016-08-17 Time: 10:22 </p>
+ *
+ * @author jiyong.jy
+ */
+public class StartWithMaven {
+
+    public static void main(String[] args) {
+        get("/hello", (req, res) -> "Hello World");
+    }
+}
